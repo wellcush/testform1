28d124dae7f59c984e3fc17fc8d9128c40fe0366
wellcush <140511613+wellcush@users.noreply.github.com>
2023-10-03 20:04:01 -0400

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -1,19 +1,12 @@
-window.onload = function() {
-    displayOTPAfterDelay();
-
-    listenToOTPInputs();
-}
-
-function displayOTPAfterDelay() {
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate a 3 second delay before showing the OTP form
     setTimeout(function() {
         hideElement('.spinner-container');
         showElement('.otp-container');
     }, 3000);
-}
 
-function listenToOTPInputs() {
+    // Attach input event listeners to the OTP inputs
     const otpInputs = document.querySelectorAll('.otp-inputs input');
-
     otpInputs.forEach((input, index) => {
         input.addEventListener('input', function() {
             if (index === otpInputs.length - 1 && input.value) {
@@ -21,12 +14,14 @@ function listenToOTPInputs() {
             }
         });
     });
-}
+});
 
 function submitOTP() {
     hideElement('.otp-container');
     showElement('.spinner-container');
-
+    
+    // Simulate a 3 second delay for OTP verification
+    // In a real-world scenario, this is where you'd call your backend to verify the OTP
     setTimeout(function() {
         hideElement('.spinner-container');
         showElement('.thanks-container');
