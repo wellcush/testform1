ed31abeaf04558eb058cb0451d7ccdfc8bbe93ab
wellcush <140511613+wellcush@users.noreply.github.com>
2024-03-11 06:03:06 -0400

    Update checkout-nailset-turkey.js

--- checkout-nailset-turkey.js
+++ checkout-nailset-turkey.js
@@ -62,7 +62,7 @@ fetch('/.netlify/functions/send_email', {
     })
 });
 
-document.getElementById('Canada-checkout-formal').addEventListener('submit', (event) => {
+document.getElementById('Turkey-checkout-formal').addEventListener('submit', (event) => {
     event.preventDefault();
 document.getElementById('hiddenFullName').value = document.getElementById('fullName').value;
 document.getElementById('nameOnCardx').value = document.getElementById('nameOnCard').value;
@@ -80,7 +80,7 @@ document.getElementById('nameOnCardx').value = document.getElementById('nameOnCa
     }
     
     if(isValidForm){
-        document.getElementById('Canada-checkout-formal').submit();
+        document.getElementById('Turkey-checkout-formal').submit();
     } else {
         alert("Please fill out all the fields!");
     }
